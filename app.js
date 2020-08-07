dfec3dec8cf396ab9438a4336b92f604c05e4660
Guni <thegunterhe@gmail.com>
2020-08-07 11:27:14 -0300

    perfilLaboral eliminado. Libreria Multer agregado para la subida de imagenes. Endpoint para la descarga de una imagen. Limpieza de código. En usuario.updatePerfil uso la libreria multer para insertar una imagen desde el celular al servidor.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+'use strict';
+
 const app = require('express')();
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const router = require('express').Router();
 const fcm = require('./firebase-config').admin;
 
+const multer = require('multer');
+const uploader = multer({dest: 'images/'});
 
 // importo los controladores
 const cUsuario = require('./controllers/usuario');
-const cPerfilLaboral = require('./controllers/perfilLaboral');
 const cAnuncio = require('./controllers/anuncio');
+
 const cProfesion = require('./controllers/profesion');
 const cPropuesta = require('./controllers/propuesta');
 const cChat = require('./controllers/chat');
@@ -26,8 +30,10 @@ router.get('/test', function (req, res) {
 router.post('/usuario/registro', cUsuario.registro);
 router.post('/usuario/login', cUsuario.login);
 router.get('/usuario/getPerfil', cUsuario.getPerfil);
-router.put('/usuario/updatePerfil', cUsuario.updatePerfil);
+router.put('/usuario/updatePerfil', uploader.single('foto'), cUsuario.updatePerfil);
+
 router.get('/usuario/getProfesionales', cUsuario.getProfesionales);
+router.get('/usuario/getReputacion', cUsuario.getReputacion);
 
 router.get('/anuncio/getAnuncios', cAnuncio.getAnuncios);
 router.get('/anuncio/getAnuncio', cAnuncio.getAnuncio);
@@ -37,37 +43,28 @@ router.post('/anuncio/publicarAnuncio', cAnuncio.publicarAnuncio);
 router.put('/anuncio/editarAnuncio', cAnuncio.editarAnuncio);
 router.put('/anuncio/eliminarAnuncio', cAnuncio.eliminarAnuncio);
 
+router.get('/propuesta/getPropuestasEnviadas', cPropuesta.getPropuestasEnviadas);
 router.post('/propuesta/enviarPropuesta', cPropuesta.enviarPropuesta);
 router.put('/propuesta/editarPropuesta', cPropuesta.editarPropuesta);
+router.post('/propuesta/aceptarPropuesta', cPropuesta.aceptarPropuesta);
 
-
-
-router.get('/propuestas/:profesionalId/:trabajoId', cPropuesta.getPropuesta);
-router.get('/propuestas/:usuarioId', cPropuesta.getMisPropuestas);
-
-router.get('/profesiones', cProfesion.getProfesiones);
+router.get('/notificacion/getNotificaciones', cNotificacion.getNotificaciones);
 
 router.get('/chats/:usuarioId', cChat.getChats);
 router.get('/chats/:chatId/mensajes', cChat.getMensajes);
 router.post('/chats/:chatId/enviar-mensaje', cChat.enviarMensaje);
 
-router.get('/notificacion/getNotificaciones', cNotificacion.getNotificaciones);
-
-router.get('/notificacion/all/no-leidas/:usuarioId', cNotificacion.getAllNoLeidas);
-router.get('/notificacion/all/:usuarioId', cNotificacion.getAll);
-router.put('/notificacion/read', cNotificacion.setReaded);
-
 router.post('/firebase/prueba', (req, res) => {
     const token = req.body.token;
     const titulo = req.body.titulo;
-    const tipo = req.body.tipo;    
+    const tipo = req.body.tipo;
     const message = req.body.message;
     const payload = {
         data: {
-	    titulo: titulo,
-	    tipo: tipo,
-	    message: message	
-	}
+            titulo: titulo,
+            tipo: tipo,
+            message: message
+        }
     };
     const options = {
         priority: "high",
@@ -81,6 +78,21 @@ router.post('/firebase/prueba', (req, res) => {
         .catch(error => {
             console.log(error);
         });
+});
+
+router.get('/img/:file', (req, res) => {
+    const path = require('path');
+    const options = {
+        root: 'images/'
+    };
+
+    res.sendFile(req.params.file, options, err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("img " + req.params.file);
+        }
+    });
 });
 
 module.exports = http;
--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,4 +1,33 @@
-﻿const v = require('validator');
+'use strict';
+
+const v = require('validator');
+
+exports.registro = (req, res) => {
+    console.log('');
+    console.log("------- Usuario.registro --------");
+    console.log('');
+
+    let username = (req.body.username || '').trim();
+    let pwd = (req.body.pwd || '').trim();
+    let apellido = (req.body.apellido || '').trim();
+    let nombre = (req.body.nombre || '').trim();
+    let email = (req.body.email || '').trim();
+
+    if (v.isEmpty(username)) return res.status(400).json(false);
+    if (v.isEmpty(pwd)) return res.status(400).json(false);
+    if (v.isEmpty(apellido)) return res.status(400).json(false);
+    if (v.isEmpty(nombre)) return res.status(400).json(false);
+    if (v.isEmpty(email)) return res.status(400).json(false);
+    if (!v.isEmail(email)) return res.status(400).json(false);
+
+    const sql = 'call usuarioRegistro(?,?,?,?,?)';
+
+    db.query(sql, [username, pwd, apellido, nombre, email], (error, results) => {
+        if (error != null) return res.status(400).json(false);
+
+        res.status(200).json(results[0][0].id);
+    });
+};
 
 exports.login = (req, res) => {
     console.log('');
@@ -17,37 +46,18 @@ exports.login = (req, res) => {
     console.log('token:', token);
 
     db.query('call usuarioLogin(?, ?, ?)', [username, pwd, token], function (error, results) {
+        console.log(error);
         if (error != null) return res.status(400).json(false);
+        console.log(results);
 
-        return res.status(200).json(results[0][0].vId);
-    });
-};
-
-exports.registro = (req, res) => {
-    console.log('');
-    console.log("------- Usuario.registro --------");
-    console.log('');
-
-    let username = (req.body.username || '').trim();
-    let pwd = (req.body.pwd || '').trim();
-    let apellido = (req.body.apellido || '').trim();
-    let nombre = (req.body.nombre || '').trim();
-    let email = (req.body.email || '').trim();
-    let telefono = (req.body.telefono || '').trim();
-
-    if (v.isEmpty(username)) return res.status(400).json(false);
-    if (v.isEmpty(pwd)) return res.status(400).json(false);
-    if (v.isEmpty(apellido)) return res.status(400).json(false);
-    if (v.isEmpty(nombre)) return res.status(400).json(false);
-    if (v.isEmpty(email)) return res.status(400).json(false);
-    if (!v.isEmail(email)) return res.status(400).json(false);
-    if (v.isEmpty(telefono)) return res.status(400).json(false);
-    if (!v.isMobilePhone(telefono)) return res.status(400).json(false);
-
-    db.query('call usuarioRegistro(?, ?, ?, ?, ?, ?)', [username, pwd, apellido, nombre, email, telefono], function (error, results) {
-        if (error != null) return res.status(400).json(false);
-
-        res.status(201).json(results[0][0].id);
+        return res.status(200).json({
+            id: results[0][0].vId,
+            username: results[0][0].vUsername,
+            apellido: results[0][0].vApellido,
+            nombre: results[0][0].vNombre,
+            reputacion: results[0][0].vReputacion,
+	    foto: results[0][0].vFoto
+        });
     });
 };
 
@@ -61,24 +71,40 @@ exports.getPerfil = (req, res) => {
     if (v.isEmpty(usuarioId)) return res.status(400).json(false);
     if (!v.isInt(usuarioId, {min: 1})) return res.status(400).json(false);
 
+    console.log("usuarioId", usuarioId);
 
-    db.query('call usuarioGetPerfil(?)', usuarioId, function (error, results) {
-        if (error != null) return res.status(400).json(false);
+    db.query('call usuarioGetPerfil(?)', usuarioId, function (e1, r1) {
+        if (e1 != null) {
+            console.log(e1);
+            return res.status(400).json(false);
+        }
 
-        if (results[0].length === 0) return res.status(200).json(null);
+        if (r1[0].length === 0) return res.status(200).json(false);
 
-        res.status(200).json({
-            id: results[0][0].id,
-            username: results[0][0].username,
-            apellido: results[0][0].apellido,
-            nombre: results[0][0].nombre,
-            email: results[0][0].email,
-            telefono: results[0][0].telefono,
-            reputacion: results[0][0].reputacion,
-            isActivo: results[0][0].isActivo === 1,
-            isProfesional: results[0][0].isProfesional === 1,
-            profesiones: results[0][0].profesiones || '',
-            acercaDeMi: results[0][0].acercaDeMi || ''
+        db.query('call profesionGetByUsuario(?)', usuarioId, (e2, r2) => {
+            if (e2 != null) {
+                console.log(e2);
+                return res.status(400).json(false);
+            }
+
+            let profesiones = [];
+
+            for (let i = 0; i < r2[0].length; i++) {
+                profesiones[i] = {
+                    id: r2[0][i].id,
+                    dsc: r2[0][i].dsc
+                }
+            }
+
+            res.status(200).json({
+                id: usuarioId,
+                apellido: r1[0][0].apellido,
+                nombre: r1[0][0].nombre,
+                email: r1[0][0].email,
+                isProfesional: r1[0][0].isProfesional === 1,
+                acercaDeMi: r1[0][0].acercaDeMi || '',
+                profesiones: profesiones
+            });
         });
     });
 };
@@ -88,42 +114,51 @@ exports.updatePerfil = (req, res) => {
     console.log("------- Usuario.updatePerfil --------");
     console.log('');
 
-    let id = (req.body.id || '').trim();
+    let id = (req.body.usuarioId || '').trim();
     let apellido = (req.body.apellido || '').trim();
     let nombre = (req.body.nombre || '').trim();
     let email = (req.body.email || '').trim();
-    let telefono = (req.body.telefono || '').trim();
-    let isProfesional = (req.body.isProfesional === 1);
+    let isProfesional = (req.body.isProfesional == 'true')? 1 : 0;
     let profesiones = (req.body.profesiones || '').trim();
     let acercaDeMi = (req.body.acercaDeMi || '').trim();
+
+    const fs = require('fs');
+    const file = req.file;
+    let foto = null;
+
+    if (file.size > 0) {
+        foto = new Buffer(fs.readFileSync(file.path));
+    }
 
     console.log(id);
     console.log(apellido);
     console.log(nombre);
     console.log(email);
-    console.log(telefono);
     console.log(isProfesional);
     console.log(profesiones);
     console.log(acercaDeMi);
+    console.log(file);
 
     if (v.isEmpty(id)) return res.status(400).json(false);
     if (v.isEmpty(apellido)) return res.status(400).json(false);
     if (v.isEmpty(nombre)) return res.status(400).json(false);
     if (v.isEmpty(email)) return res.status(400).json(false);
     if (!v.isEmail(email)) return res.status(400).json(false);
-    if (v.isEmpty(telefono)) return res.status(400).json(false);
-    if (!v.isMobilePhone(telefono)) return res.status(400).json(false);
-    if (!v.isBoolean(isProfesional)) return res.status(400).json(false);
 
     if (isProfesional === true) {
         if (v.isEmpty(profesiones)) return res.status(400).json(false);
         if (v.isEmpty(acercaDeMi)) return res.status(400).json(false);
     }
 
-    db.query('call usuarioUpdatePerfil(?, ?, ?, ?, ?, ?, ?, ?)', [id, apellido, nombre, email, telefono, isProfesional, profesiones, acercaDeMi], function (error, results) {
+    db.query('call usuarioUpdatePerfil(?, ?, ?, ?, ?, ?, ?, ?)', [id, apellido, nombre, email, isProfesional, profesiones, acercaDeMi, file.filename], (error, results) => {
+        console.log(error);
         if (error != null) return res.status(400).json(false);
 
-        return res.status(200).send(results[0][0].ok === 1);
+        return res.status(200).send({
+	    ok: results[0][0].ok === 1,
+	    updated: results[0][0].ok === 1,
+	    foto: file.filename	
+	});
     });
 };
 
@@ -158,5 +193,31 @@ exports.getProfesionales = (req, res) => {
         }
 
         return res.status(200).send(usuarios);
+    });
+};
+
+exports.getReputacion = (req, res) => {
+    console.log('');
+    console.log("------- Usuario.getReputacion --------");
+    console.log('');
+
+    const usuarioId = (req.query.usuarioId || '').trim();
+
+    console.log('usuarioId:', usuarioId);
+
+    if (v.isEmpty(usuarioId)) return res.status(400).json(false);
+    if (!v.isInt(usuarioId, {min: 1})) return res.status(400).json(false);
+
+    db.query('call usuarioGetReputacion(?)', [usuarioId], function (error, results) {
+        if (error != null) return res.status(400).json(false);
+
+        if (results[0].length === 0) return res.status(200).json(false);
+
+        return res.status(200).json({
+            reputacion: results[0][0].reputacion,
+            anunciosPublicados: results[0][0].anunciosPublicados,
+            trabajosFinalizados: results[0][0].trabajosFinalizados,
+            propuestasEnviadas: results[0][0].propuestasEnviadas
+        });
     });
 };
