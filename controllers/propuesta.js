ec47e5bca44882d47fa9a3f352cde3510c4e8511
Guni <hellriglgunter@gmail.com>
2020-08-20 23:25:03 -0300

    Funciones descomentadas

--- controllers/propuesta.js
+++ controllers/propuesta.js
@@ -89,14 +89,14 @@ const fcm = require('../firebase-config').admin;
 // };
 
 exports.getPropuestasEnviadas = (req, res) => {
-	console.log('');
+    console.log('');
     console.log("------- Propuesta.getPropuestasEnviadas --------");
     console.log('');
 
-	// let usuarioId = (req.query.usuarioId || '').trim();
-	// console.log(usuarioId);
+    const usuarioId = (req.query.usuarioId || '').trim();
+    console.log(usuarioId);
     //
-	// if (v.isEmpty(usuarioId)) return res.status(200).send({
+    // if (v.isEmpty(usuarioId)) return res.status(200).send({
     //     ok: false,
     //     message: "usuarioId está vacío"
     // });
@@ -105,82 +105,84 @@ exports.getPropuestasEnviadas = (req, res) => {
     //     message: 'usuarioId con formato incorrecto'
     // });
     //
-	// db.query('call propuestaGetPropuestasEnviadas(?)', [usuarioId], (error, results) => {
-    //     if (error != null) {
-    //         console.log('Error: ', error);
-    //         return res.status(400).json(false);
-    //     }
-    //
-	// 	console.log(results[0]);
-    //
-	// 	let propuestas = [];
-    //
-	// 	for (let i = 0; i < results[0].length; i++) {
-	// 		propuestas[i] = {
-	// 			id: results[0][i].id,
-	// 			anuncio: {
-	// 				titulo: results[0][i].titulo
-	// 			},
-	// 			profesional: {
-	// 				id: results[0][i].profesionalId
-	// 			}
-	// 		}
-	// 	}
-    //
-    //     res.status(200).json(propuestas);
-    // });
+    db.query('call propuestaGetPropuestasEnviadas(?)', usuarioId, (error, results) => {
+        if (error != null) {
+            console.log('Error: ', error);
+            return res.status(400).json(false);
+        }
+
+        console.log(results[0]);
+
+        let propuestas = [];
+
+        for (let i = 0; i < results[0].length; i++) {
+            propuestas[i] = {
+                id: results[0][i].id,
+                dsc: results[0][i].dsc,
+                anuncio: {
+                    titulo: results[0][i].titulo
+                },
+                profesional: {
+                    id: results[0][i].profesionalId
+                }
+            }
+        }
+
+        res.status(200).json(propuestas);
+    });
 }
 
 exports.enviarPropuesta = (req, res) => {
     console.log('');
     console.log("------- Propuesta.enviarPropuesta --------");
     console.log('');
-    //
-    // const data = [
-    //     anuncioId = (req.body.anuncioId || '').trim(),
-    //     profesionalId = (req.body.profesionalId || '').trim(),
-    //     dsc = (req.body.dsc || '').trim(),
-    //     precio = (req.body.id || '0').trim()
-    // ];
-    //
+
+    const anuncioId = (req.body.anuncioId || '').trim();
+    const profesionalId = (req.body.profesionalId || '').trim();
+    const dsc = (req.body.dsc || '').trim();
+    const precio = (req.body.id || '0').trim();
+
     // if (v.isEmpty(data[0])) return res.status(400).json(false);
     // if (v.isEmpty(data[1])) return res.status(400).json(false);
     // if (v.isEmpty(data[2])) return res.status(400).json(false);
     // if (!v.isEmpty(data[3]) && !v.isDecimal(data[3])) return res.status(400).json(false);
     //
-    // db.query('call propuestaInsert(?,?,?,?)', data, (err, r1) => {
-    //     if (err) return res.status(400).json(false);
-    //
-    //     res.status(200).json(r1[0][0].id);
-    //
-    //     db.query('call getDataFromAnuncioToNotify(?, ?)', [data[0], data[1]], (e2, r2) => {
-    //         const token = r2[0][0].token || '';
-    //         const anuncioTitulo = r2[0][0].titulo || '';
-    //         const profesionalUsername = r2[0][0].username || '';
-    //
-    //         const payload = {
-    //             data: {
-    //                 action: 'notification',
-    //                 tipo: '1',
-    //                 drawable: 'ic_work',
-    //                 title: anuncioTitulo,
-    //                 contentText: "@" + profesionalUsername + " te ha enviado una propuesta"
-    //             }
-    //         };
-    //         const options = {
-    //             priority: "high",
-    //             timeToLive: 60 * 60 * 24
-    //         };
-    //
-    //         fcm.messaging().sendToDevice(token, payload, options)
-    //             .then(response => {
-    //                 console.log('Action executed');
-    //             })
-    //             .catch(error => {
-    //                 console.log(error);
-    //             });
-    //     });
-    // });
+    db.query('call propuestaInsert(?,?,?,?)', [anuncioId, profesionalId, dsc, precio], (e1, r1) => {
+        if (e1) {
+            console.log("Error:", e1)
+            return res.status(400).json(false);
+        }
+
+        res.status(200).json(r1[0][0].id);
+
+        // db.query('call getDataFromAnuncioToNotify(?, ?)', [data[0], data[1]], (e2, r2) => {
+        //     const token = r2[0][0].token || '';
+        //     const anuncioTitulo = r2[0][0].titulo || '';
+        //     const profesionalUsername = r2[0][0].username || '';
+        //
+        //     const payload = {
+        //         data: {
+        //             action: 'notification',
+        //             tipo: '1',
+        //             drawable: 'ic_work',
+        //             title: anuncioTitulo,
+        //             contentText: "@" + profesionalUsername + " te ha enviado una propuesta"
+        //         }
+        //     };
+        //     const options = {
+        //         priority: "high",
+        //         timeToLive: 60 * 60 * 24
+        //     };
+        //
+        //     fcm.messaging().sendToDevice(token, payload, options)
+        //         .then(response => {
+        //             console.log('Action executed');
+        //         })
+        //         .catch(error => {
+        //             console.log(error);
+        //         });
+        // });
+    });
 };
 
 exports.editarPropuesta = (req, res) => {
@@ -188,21 +190,22 @@ exports.editarPropuesta = (req, res) => {
     console.log("------- Propuesta.editarPropuesta --------");
     console.log('');
 
-    // const data = [
-    //     id = (req.body.id || '').trim(),
-    //     dsc = (req.body.dsc || '').trim(),
-    //     precio = (req.body.id || '0').trim()
-    // ];
-    //
+    const id = (req.body.id || '').trim();
+    const dsc = (req.body.dsc || '').trim();
+    const precio = (req.body.precio || '0').trim();
+
     // if (v.isEmpty(data[0])) return res.status(400).json(false);
     // if (v.isEmpty(data[1])) return res.status(400).json(false);
     // if (!v.isEmpty(data[2]) && !v.isDecimal(data[2])) return res.status(400).json(false);
     //
-    // db.query('call propuestaUpdate(?,?,?)', data, (err, r1) => {
-    //     if (err) return res.status(400).json(false);
-    //
-    //     res.status(200).json(r1[0][0].ok === 1);
-    // });
+    db.query('call propuestaUpdate(?,?,?)', [id, dsc, precio], (e1, r1) => {
+        if (e1) {
+            console.log("Error:", e1);
+            return res.status(400).json(false);
+        }
+
+        res.status(200).json(r1[0][0].ok === 1);
+    });
 };
 
 exports.aceptarPropuesta = (req, res) => {
@@ -221,14 +224,14 @@ exports.aceptarPropuesta = (req, res) => {
     //     if (e1) return res.status(400).json(false);
     //
     //     res.status(200).json({
-	// 		id: r1[0][0].chatId,
-	// 		emisor: {
-	// 			id: r1[0][0].emisorId
-	// 		},
-	// 		receptor: {
-	// 			id: r1[0][0].receptorId
-	// 		}
-	// 	});
+    // 		id: r1[0][0].chatId,
+    // 		emisor: {
+    // 			id: r1[0][0].emisorId
+    // 		},
+    // 		receptor: {
+    // 			id: r1[0][0].receptorId
+    // 		}
+    // 	});
     //
     //     db.query('call getDataFromPropuestaToNotify(?)', propuestaId, (e2, r2) => {
     //     	console.log('e2:', e2);
