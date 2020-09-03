a6415bd9fb1d2d71df03b9bdcef14151cd5f69a1
Guni <hellriglgunter@gmail.com>
2020-09-02 21:01:35 -0300

    comentarios eliminados, metodos de getNotificaciones y setLeido habilitados

--- controllers/notificacion.js
+++ controllers/notificacion.js
@@ -1,137 +1,57 @@
-'use strict'
+'use strict';
 
 exports.getNotificaciones = (req, res) => {
     console.log('');
     console.log("------- Notificacion.getNotificaciones --------");
     console.log('');
 
-//     const usuarioId = (req.query.usuarioId || '').trim();
-//
-//     console.log("usuarioId:", usuarioId);
-//
-//     if (v.isEmpty(usuarioId)) return res.status(400).json(false);
-//     if (!v.isInt(usuarioId, {min:1})) return res.status(400).json(false);
-//
-//     db.query('call notificacionGet(?)', usuarioId, (error, results) => {
-//         if (error != null) return res.status(400).json(false);
-//
-// 		let notificaciones = [];
-//
-// 		for(let i = 0; i < results[0].length; i++) {
-// 			notificaciones[i] = {
-// 				id: results[0][i].id,
-// 				usuario: {
-// 					id: results[0][i].usuarioId
-// 				},
-// 				titulo: results[0][i].titulo,
-// 				fechaHora: results[0][i].fechaHora,
-// 				leido: results[0][i].leido === 1,
-// 				tipo: results[0][i].tipo
-// 			}
-// 		}
-// console.log(notificaciones);
-//
-//         return res.status(200).json(notificaciones);
-//     });
+    const usuarioId = (req.query.usuarioId || '').trim();
+
+    console.log("usuarioId:", usuarioId);
+
+    db.query('call notificacionGetNotificaciones(?)', usuarioId, (error, results) => {
+        if (error != null) {
+            console.log("Error", error);
+            return res.status(400).json(false);
+        }
+
+        let notificaciones = [];
+
+        for (let i = 0; i < results[0].length; i++) {
+            notificaciones[i] = {
+                id: results[0][i].id,
+                usuario: {
+                    id: results[0][i].usuarioId
+                },
+                titulo: results[0][i].titulo,
+                fechaHora: results[0][i].fechaHora,
+                isLeido: results[0][i].isLeido === 1,
+                tipo: results[0][i].tipo,
+                data: results[0][i].data
+            }
+        }
+
+        console.log(notificaciones);
+
+        return res.status(200).json(notificaciones);
+    });
 };
 
-exports.getAllNoLeidas = (req, res) => {
-	console.log('');
-    console.log("------- Notificacion - getAllNoLeidas --------");
+exports.setLeido = (req, res) => {
+    console.log('');
+    console.log("------- Notificacion.setLeido --------");
     console.log('');
 
-    // let usuarioId = (req.params.usuarioId || '').trim();
-    //
-    // if (v.isEmpty(usuarioId)) return res.status(200).send({
-    //     ok: false,
-    //     message: "usuarioId está vacío"
-    // });
-    // if (!v.isInt(usuarioId, {min:1})) return res.status(400).send({
-    //     ok: false,
-    //     message: 'usuarioId con formato incorrecto'
-    // });
-    //
-    //
-    // db.query('call getNotificacionesNoLeidas(?)', [usuarioId], (error, results) => {
-    // 	if (error != null) {
-    //         console.log('Error: ', error);
-    //         return res.status(500).send({error});
-    //     }
-    //
-    //     if (results == null) return res.status(200).send({ok:false});
-    //
-    //     console.log(results[0]);
-    //
-    //     return res.status(200).send({
-    //         ok: true,
-    //         notificaciones: results[0]
-    //     });
-    // });
-};
+    const id = (req.body.id || '').trim();
 
-exports.getAll = (req, res) => {
-    console.log('');
-    console.log("------- Notificacion - getAll --------");
-    console.log('');
+    console.log('id', id);
 
-    // let usuarioId = (req.params.usuarioId || '').trim();
-    //
-    // if (v.isEmpty(usuarioId)) return res.status(200).send({
-    //     ok: false,
-    //     message: "usuarioId está vacío"
-    // });
-    // if (!v.isInt(usuarioId, {min:1})) return res.status(400).send({
-    //     ok: false,
-    //     message: 'usuarioId con formato incorrecto'
-    // });
-    //
-    //
-    // db.query('call getNotificaciones(?)', [usuarioId], (error, results) => {
-    //     if (error != null) {
-    //         console.log('Error: ', error);
-    //         return res.status(500).send({error});
-    //     }
-    //
-    //     if (results == null) return res.status(200).send({ok:false});
-    //
-    //     console.log(results[0]);
-    //
-    //     return res.status(200).send({
-    //         ok: true,
-    //         notificaciones: results[0]
-    //     });
-    // });
-};
+    db.query('call notificacionSetLeido(?)', id, (error, results) => {
+        if (error != null) {
+            console.log('Error: ', error);
+            return res.status(400).json(false);
+        }
 
-exports.setReaded = (req, res) => {
-    console.log('');
-    console.log("------- Notificacion - setReaded --------");
-    console.log('');
-
-    // let id = (req.body.id || '').trim();
-    //
-    // if (v.isEmpty(id)) return res.status(200).send({
-    //     ok: false,
-    //     message: "id está vacío"
-    // });
-    // if (!v.isInt(id, {min:1})) return res.status(400).send({
-    //     ok: false,
-    //     message: 'id con formato incorrecto'
-    // });
-    //
-    //
-    // db.query('call updateNotificacion(?)', [id], (error, results) => {
-    //     if (error != null) {
-    //         console.log('Error: ', error);
-    //         return res.status(500).send({error});
-    //     }
-    //
-    //     if (results == null) return res.status(200).send({ok:false});
-    //
-    //     console.log(results[0]);
-    //
-    //     return res.status(200).send({
-    //         ok: true
-    //     });
-    // });
+        res.status(200).json(results[0][0].ok === 1);
+    });
 };
